02130cb9fd3e167fa255f721ce7b8406283f425b
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-06-20 19:02:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
     @num_of_words = @analyzed_text.count_of_words
     @vowels = @analyzed_text.count_of_vowels
     @consonants = @analyzed_text.count_of_consonants
-    puts @analyzed_text.most_used_letter[:i]
+    puts @analyzed_text.most_used_letter[:i].to_s
     erb :results
   end
 end
